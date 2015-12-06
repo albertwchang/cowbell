95f1c7bbaa61f5082f0cbbfe2079249c5d59f7e0
Albert W. Chang <albertwchang@gmail.com>
2015-12-06 02:24:47 -0800

    trying to fix details so that Issue List can display properly.

--- Stores/IssueStore.js
+++ Stores/IssueStore.js
@@ -57,16 +57,16 @@ var IssueStore = Reflux.createStore({
 		}
 	},
 
-	_assignIssue: function(issueRef, entityType) {
-		var issueId = issueRef.key();
-		var issue = issueRef.val();
-		var existingissues = this._issues[entityType]; // issues is NOT an array!!!
+	_assignIssue: function(issueRef, perspective) {
+		let issueId = issueRef.key();
+		let issue = issueRef.val();
+		let existingissues = this._issues[perspective]; // issues is NOT an array!!!
 		
 		if ( existingIssues[issueId] && !_.eq(existingIssues[issueId].statusEntries, issue.statusEntries) )
 			issue.statusEntries = this._scrubStatusEntries(issue.statusEntries, ["read", "status"]);
 
 		existingIssues[issueId] = issue;
-		this._updateIssueList(existingIssues, entityType);
+		this._updateIssueList(existingIssues, perspective);
 	},
 
 	_filterIssues: function(issues) {
@@ -80,21 +80,21 @@ var IssueStore = Reflux.createStore({
 		*/
 
 		return _.filter(issues, (issue) => {
-			var includeIssue = false;
+			let includeIssue = false;
 
-			if (_.isEmpty(Issue))
+			if (_.isEmpty(issue))
 				return includeIssue;
 			
-			var siteRight = this._currentSiteRight;
-			var lastStatusRef = this._lookups.statuses[_.last(issue.statusEntries).statusId];
-			var user = this._currentUser;
-			var filterState = user.settings.filters.statuses.states
+			let siteRight = this._currentSiteRight
+        , lastStatusRef = this._lookups.statuses[_.last(issue.statusEntries).statusId]
+        , user = this._currentUser
+        , filterState = user.settings.filters.statuses.states;
 			
 			if ( _.has(lastStatusRef, "nextStatuses") ) {
-				var nextStatusIds = _.pluck(lastStatusRef.nextStatuses, "statusId");
+				let nextStatusIds = _.pluck(lastStatusRef.nextStatuses, "statusId");
 					
 				_.each(nextStatusIds, (statusId) => {
-					var nextStatusRef = this._lookups.statuses[statusId];
+					let nextStatusRef = this._lookups.statuses[statusId];
 					
 					includeIssue = nextStatusRef.accessRights.read.status[siteRight.orgTypeId]
 							? filterState[IssueMixin.Filters.OPEN]
@@ -108,7 +108,7 @@ var IssueStore = Reflux.createStore({
 	},
 
 	_mapVehicleData: function(vehicleData) {
-		var dataMap = this._lookups.hosts.vehicle["dataMap"];
+		let dataMap = this._lookups.hosts.vehicle["dataMap"];
     
     return {
   		drivetrain: dataMap.drivetrain[vehicleData.drivenWheels] || "",
@@ -120,40 +120,27 @@ var IssueStore = Reflux.createStore({
   	}
   },
 
-  _scrubStatusEntries: function(statusEntries, params) {
-    var currentOrgTypeId = this._currentSiteRight.orgTypeId
-    var scrubbedStatusEntries = _.filter(statusEntries, (statusEntry) => {
-      var statusRef = this._lookups.statuses[statusEntry.statusId];
-      // var isStatusAllowed = (statusLookup.accessRights[currentOrgTypeId].status[action] === true);
-      
-      var isStatusAllowed = _.get(statusRef.accessRights, params)[currentOrgTypeId];
-
-      return isStatusAllowed;
-    });
-
-		return scrubbedStatusEntries;
-	},
-
   _sortIssues: function(issues) {
-  	var now = Moment();
+  	let now = Moment();
 
-  	var sortedIssues = _.sortBy(issues, (issue) => {
-  		var statusEntry = _.last(issue.statusEntries);
-  		var diff = now.diff(Moment(statusEntry.timestamp), "seconds");
-  		return diff;
+  	let sortedIssues = _.sortBy(issues, (issue) => {
+  		let statusEntry = _.last(issue.statusEntries)
+  		  , diff = now.diff(Moment(statusEntry.timestamp), "seconds");
+  		
+      return diff;
   	});
 
   	return sortedIssues;
   },
 
-  _updateIssueList: function(issues, entityType) {
+  _updateIssueList: function(issues, perspective) {
   	// 1. Update model
   	if (!issues)
-  		issues = this._issues[entityType];
+  		issues = this._issues[perspective];
   	else
-  		this._issues[entityType] = issues;
+  		this._issues[perspective] = issues;
 
-  	var filteredIssues = {};
+  	let filteredIssues = {};
 
   	// 2. Filter issues based on selected filter
   	issues = this._filterIssues(_.toArray(issues));
@@ -169,7 +156,7 @@ var IssueStore = Reflux.createStore({
   },
 
   _urlForQuery: function(url, params) {
-		var queryString = Object.keys(params).map(key => key + "=" +encodeURIComponent(params[key])).join("&");		
+		let queryString = Object.keys(params).map(key => key + "=" +encodeURIComponent(params[key])).join("&");		
 		return url +queryString;
 	},
 
@@ -204,8 +191,8 @@ var IssueStore = Reflux.createStore({
 	},
 
 	onRecordImg: function(issue, imgObj) {
-  	let qUpload = IssueActions.uploadImg.triggerPromise(imgObj);
-  	let params = ["images", issue.images.length]
+  	let qUpload = IssueActions.uploadImg.triggerPromise(imgObj)
+      , params = ["images", issue.images.length]
   		, qRecord = IssueActions.setParam(issue, params, imgObj.dbRecord);
 
   	new Promise.all([qUpload, qRecord]).then((results) => {
@@ -218,8 +205,8 @@ var IssueStore = Reflux.createStore({
 	},
 
 	onAddIssue: function(newIssue) {
-		var issuesRef = this._db;
-		var issueRef = issuesRef.push(newIssue);
+		let issuesRef = this._db;
+		let issueRef = issuesRef.push(newIssue);
 
 		issueRef.update({"iid": issueRef.key()}, (err) => {
 			if (err)
@@ -231,13 +218,13 @@ var IssueStore = Reflux.createStore({
 
 	onAddStatus: function(nextStatus, issue, notes, sites) {
 		console.log("issueId: ", issue.iid);
-		// var issueStatusEntriesRef = this._db.child(issue.iid).child("statusEntries");
-		var siteRight = this._currentSiteRight;
-		var newUser = this._currentUser;
+		// let issueStatusEntriesRef = this._db.child(issue.iid).child("statusEntries");
+		let siteRight = this._currentSiteRight
+		  , newUser = this._currentUser;
     
     LocationActions.getPosition.triggerPromise().then((position) => {
-	    var prevUserId = _.last(issue.statusEntries).author.id;
-	    var statusEntry = {
+	    let prevUserId = _.last(issue.statusEntries).author.id;
+	    let statusEntry = {
 	      timestamp: Moment(Moment().toDate()).format(),
 	      geoPoint: position,
 	      statusId: nextStatus.iid,
@@ -269,11 +256,11 @@ var IssueStore = Reflux.createStore({
 					// 2. Add issue Id to all other allies' issue list
 		    	if ( nextStatus.assignTo[this._orgTypeIds.VENDOR].site ) {
 		    		// iterate through all ally orgTypes of client
-		    		var allySites = _.omit(sites, this._orgTypeIds.CLIENT)
+		    		let allySites = _.omit(sites, this._orgTypeIds.CLIENT)
 		    			, qAllySites = [];
 
 						_.each(allySites, (allySite, key) => {
-							var qAllySite = new Promise((resolve, reject) => {
+							let qAllySite = new Promise((resolve, reject) => {
 								Async.parallel([
 									(issueIdCb) => {
 										SiteActions.setIssueId.triggerPromise(issue.iid, allySite.iid, key).then(() => {
@@ -322,21 +309,21 @@ var IssueStore = Reflux.createStore({
 		/*************************************************************************
 		 If S3 policy times out, error callback will have to obtain a new policy
 		*************************************************************************/
-		var beg = imgUri.lastIndexOf('/') +1
+		let beg = imgUri.lastIndexOf('/') +1
 			, end = imgUri.lastIndexOf('.')
 			, fileExt = imgUri.substr(end +1);
-    var userId = this._currentUser.iid;
+    let userId = this._currentUser.iid;
    	
    	LocationActions.getPosition.triggerPromise().then((position) => {
-   		var geoPoint = {
+   		let geoPoint = {
    			lat: position.lat,
    			latitude: position.lat,
    			long: position.long,
    			longitude: position.long
    		};
 
-   		var filename = this._buildImgFilename(Moment().format("X"),userId,imgTypeId,fileExt);  		
-   		var stagedImg = {
+   		let filename = this._buildImgFilename(Moment().format("X"),userId,imgTypeId,fileExt);  		
+   		let stagedImg = {
 	  		dbRecord: {
 		      authorId: userId,
 		      uri: this._images.folderpath +filename,
@@ -375,50 +362,29 @@ var IssueStore = Reflux.createStore({
 	},
 
 	onExtractNextStatuses: function(statusEntry, issueId) {
-		var self = this;
-		var assignedToOtherIssue = function(issueId) {
-      var assignmentStatus;
-      var userState = self._currentUser.state;
+		let self = this;
 
-      switch(self._currentSiteRight.orgTypeId) {
-        case SiteMixin._orgTypeIds.VENDOR:
-          assignmentStatus = (userState.issueId != "");
-          break;
-       	case SiteMixin._orgTypeIds.CLIENT:
-          assignmentStatus = false;
-          break;
-      }
-
-      return assignmentStatus;
+		let assignedToOtherIssue = function(issueId) {
+      let assignmentStatus
+        , userState = self._currentUser.state;
+      
+      return !_.isEmpty(userState.issueId);
     };
 
-  	var assignedToThisIssue = function(issueId) {
-      var assignmentStatus;
-      var userState = self._currentUser.state;
-
-      switch(self._currentSiteRight.orgTypeId) {
-        case SiteMixin._orgTypeIds.VENDOR:
-          assignmentStatus = (userState.issueId == issueId);
-          break;
-        
-        case SiteMixin._orgTypeIds.CLIENT:
-          assignmentStatus = true;
-          break;
-      }
-
-      return assignmentStatus;
+  	let assignedToThisIssue = function(issueId) {
+      let assignmentStatus
+        , userState = self._currentUser.state;
+      
+      return (userState.issueId === issueId);
     };
 
-    var checkUserRights = function(prevStatusRef, nextStatusRef) {
-	    var statusRef = self._lookups.statuses[nextStatusRef.statusId];
-	    var writeRight = statusRef.accessRights.write;
-	    var taskRefs = self._lookups.tasks;
-	    var allowed;
+    let checkUserRights = function(prevStatusRef, nextStatusRef) {
+	    let statusRef = self._lookups.statuses[nextStatusRef.statusId]
+        , writeRight = statusRef.accessRights.write
+        , taskRefs = self._lookups.tasks
+        , allowed;
 	    
-	    if ( writeRight.status[self._currentSiteRight.orgTypeId] === true
-	    	// && _.contains(self._currentSiteRight.tasks, IssueMixin.TaskIds.UPDATE_STATUS) ) {
-				&& (writeRight.task === null || _.contains(self._currentSiteRight.tasks, writeRight.task)) ) {
-
+	    if ( _.has(writeRight, "task") && _.contains(self._currentSiteRight.tasks, writeRight.task) ) {
 	      // check lock status
 	      if (prevStatusRef.lockForUser)
 	        allowed = assignedToThisIssue(issueId);
@@ -431,49 +397,48 @@ var IssueStore = Reflux.createStore({
 	    return allowed ? statusRef : undefined;
 	  };
 
-    var statusRef = this._lookups.statuses[statusEntry.statusId];
-    var nextStatuses = null;
+    let statusRef = this._lookups.statuses[statusEntry.statusId]
+      , nextStatuses = null;
 
-    if ( _.has(statusRef, "nextStatuses") ) {
+    if ( _.has(statusRef, "nextStatuses") )
     	nextStatuses = _.transform(statusRef.nextStatuses, (result, nextStatusRef) => {
-      	var nextStatus = checkUserRights(statusRef, nextStatusRef);
+      	let nextStatus = checkUserRights(statusRef, nextStatusRef);
       
-	      if (nextStatus)
+	      if ( !_.isEmpty(nextStatus) )
 	        return result.push(nextStatus);
 	    }) || new Array(0);
 
-	    IssueActions.extractNextStatuses.completed(nextStatuses);
-    } else
-    	IssueActions.extractNextStatuses.completed(nextStatuses);
+    IssueActions.extractNextStatuses.completed(nextStatuses);
   },
 
   onGetIssue: function(issueId, ) {
-  	var issue = this._issues[issueId];
+  	let issue = this._issues[issueId];
   	IssueActions.getissue.completed(issue);
   },
 
-  onPullIssue: function(issueId, entityType) {
-		if (!this._issues[entityType])
+  onPullIssue: function(issueId, perspective) {
+		if (!this._issues[perspective])
 			return;
 
-		var dbRef = this._db.child(issueId);
+		let dbRef = this._db.child(issueId);
   	
   	dbRef.once("value", (issueRef) => {
-			this._assignIssue(issueRef, entityType);
+			this._assignIssue(issueRef, perspective);
 		});
   },
 
   // 1. One-time pull of existing tow issue base
   // 2. Setup listener for any updates made to existing tow issues
-	onPullIssues: function(issueIds, entityType) {
-		var qIssues = Defer();
-		var siteRight = this._currentSiteRight;
-		var dbRef = this._db.orderByChild("sites/" +siteRight.orgTypeId +"/siteId").equalTo(siteRight.siteId);
-		this._dbRefs.push(dbRef);
+	onPullIssues: function(issueIds, perspective) {
+		let qIssues = Defer()
+		  , siteRight = this._currentSiteRight
+		  , issuesRef = this._db.orderByChild("siteId").equalTo(siteRight.siteId);
 		
-		dbRef.once("value", (allIssues) => {
-			var issues = allIssues.val();
-			var issueIds = _.keys(issues);
+    this._dbRefs.push(issuesRef);
+		
+		issuesRef.once("value", (allIssues) => {
+			let issues = allIssues.val() || {}
+        , issueIds = _.keys(issues);
 			/*
 				Run sorting and filter functions
 				1. Scrub Tow issues
@@ -481,56 +446,29 @@ var IssueStore = Reflux.createStore({
 				3. Sort filtered issues
 			*/
 
-			if (!issueIds || issueIds.length === 0)
-				issues = {};
-			else
-				_.each(issueIds, (issueId) => {
-					var issue = issues[issueId];
-					issue.statusEntries = this._scrubStatusEntries(issue.statusEntries, ["read", "status"]);
-					issues[issueId] = issue;
-				});
-
-			this._updateIssueList(issues, entityType);
-			IssueActions.pullIssues.completed(this._issues[entityType] = issues);
+			this._updateIssueList(issues, perspective);
+			IssueActions.pullIssues.completed(this._issues[perspective] = issues);
 			qIssues.resolve();
 		});
 
 		// handle updated tow issues
 		qIssues.promise.then(() => {
-			dbRef.on("child_changed", (snapshot) => this._assignIssue(snapshot, entityType));
+			dbRef.on("child_changed", (snapshot) => this._assignIssue(snapshot, perspective));
 		});
 	},
 
-  onPullVehicleData: function(vin) {
-    var host = this._lookups.hosts["vehicle"];    
-    var url = host.url +vin +"?";
-    var query = this._urlForQuery(url, host.params["url"]);
-
-    fetch(query, host.params["connection"])
-      .then((res) => {
-        if (res.status === 200) {
-          var data = JSON.parse(res._bodyText);
-          IssueActions.pullVehicleData.completed(this._mapVehicleData(data));
-        } else {
-          IssueActions.pullVehicleData.failed();  
-        }
-      }).catch((err) => {
-        IssueActions.pullVehicleData.failed();
-      });
-  },
-
-  onRefreshIssues: function(entityType) {
-		this._updateIssueList(null, entityType);
+  onRefreshIssues: function(perspective) {
+		this._updateIssueList(null, perspective);
   },
 
 	onRemoveFromImages: function(issue, imgTypeId) {
-		var imagesRef = this._db.child(issue.iid).child("images");
+		let imagesRef = this._db.child(issue.iid).child("images");
 
 		imagesRef.transaction((prevList) => {
 		  if (!prevList)
 		  	return null;
 		  else {
-		  	var newList = _.remove(prevList, (image) => {
+		  	let newList = _.remove(prevList, (image) => {
 		  		return image.imgTypeId !== imgTypeId;
 		  	});
 		  	
@@ -552,11 +490,11 @@ var IssueStore = Reflux.createStore({
 	},
 
 	onSetParam: function(issue, params, value) {
-		var ref = this._db.child(issue.iid);
-		var lastIndex = _.last(params);
-		var action, arg;
+		let ref = this._db.child(issue.iid)
+		  , lastIndex = _.last(params)
+		  , action, arg;
 		
-		if (_.isNumber(lastIndex)) {
+		if ( _.isNumber(lastIndex) ) {
 			action = "transaction";
 			arg = function(prevList) {
 				if (!prevList)
@@ -579,48 +517,8 @@ var IssueStore = Reflux.createStore({
 		ref[action](arg, (err) => {
 			if (err)
 				IssueActions.setParam.failed(err);
-			
-			var todoTriggerDef = this._lookups.todos.triggers[_.first(params)]
-				, todoTrigger = todoTriggerDef ? this.getTodoTrigger(todoTriggerDef, action, value) : null;
-
-			if (_.isEmpty(todoTrigger))
+			else
 				IssueActions.setParam.completed();
-			else
-				IssueActions.setTodoStatus.triggerPromise(issue, todoTrigger)
-					.then(() => {
-						IssueActions.setParam.completed();
-					}).catch((err) => {
-						IssueActions.setParam.failed();
-					});
-		});
-	},
-
-	onSetTodoStatus: function(issue, trigger) {
-		_.each(trigger.todos, (todoId) => {
-			issue.todoMap[todoId][trigger.state] = trigger.value;
-		});
-		
-		IssueActions.setParam.triggerPromise(issue, ["todoMap"], issue.todoMap)
-			.then(() => {
-				IssueActions.setTodoStatus.completed();
-			}).catch((err) => {
-				IssueActions.setTodoStatus.failed(err);
-			});
-	},
-
-	onUpdateVehicle: function(issueId, params, newValue) {
-		var vehicleRef = this._db.child(issueId).child("vehicle");
-		var paramRef = vehicleRef;
-
-		_.each(params, (param) => {
-			paramRef = paramRef.child(param);
-		});
-
-		paramRef.set(newValue, (err) => {
-			if (err)
-				IssueActions.updateVehicle.failed(err);
-			else
-				IssueActions.updateVehicle.completed();
 		});
 	},
 
